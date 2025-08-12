c6c65e92cc47c285a9aee4fb8fce0124d7a8f77c
Georgy Thomas Geo <georgythomas04@gmail.com>
2025-08-12 06:49:05 +0530

    Update NAÏVE BAYES.py

--- NAÏVE BAYES.py
+++ NAÏVE BAYES.py
@@ -6,26 +6,27 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('iris.data.csv', header=None)
-df.shape
-df.head(10)
+df = df.drop(columns=['Id']) 
+print(df.shape)
+print(df.head(10))
 print(df)
 
 df.info()
 
 df.isnull().sum()
 
-X = df.iloc[:, :- 1].values
-y = df.iloc[:, -1].values 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 nb_classifier = GaussianNB()
 nb_classifier.fit(X_train, y_train)
 y_pred = nb_classifier.predict(X_test)
 
-accuracy - accuracy_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
 report = classification_report(y_test, y_pred)
-print (f'Accuracy: {accuracy * 100 :. 2f}%' )
+print (f'Accuracy: {accuracy * 100 :.2f}%' )
 print(f'\nClassification Report:\n{report}')
-conf_matrix - confusion_matrix(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(8,6))
 sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues',
 xticklabels=['Setosa', 'Versicolor', 'Virginica'],
